0ca748e33cd3d3ef5f097a9cd47806e799c69512
Akshat kumar <theyashgreat@gmail.com>
2022-03-03 00:22:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import UseEffectAPI from "./Component/UseEffectAPI";
+
+function App() {
+  return (
+    <>
+    <UseEffectAPI/>
+    </>
+  );
+}
+
+export default App;
+ 
